08a56e540ba1ffa88d0eba5cf0032d08c877bc52
imravikrish <ravikrishr155@gmail.com>
2022-07-16 20:47:56 +0530

    uploading Angular Quiz App

---
+++ src/app/service/question-service.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  getQuestionjson(){
+    return this.http.get<any>("assets/questions.json");
+  }
+
+}
